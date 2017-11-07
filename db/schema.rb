2dc873515d8e38b0dc159537409a613dbab16bc3
jlstafford <jlowellstafford@gmail.com>
2017-11-07 09:11:18 -0700

    all stories completed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150917195900) do
+ActiveRecord::Schema.define(version: 20171106233036) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -53,6 +53,7 @@ ActiveRecord::Schema.define(version: 20150917195900) do
     t.integer  "avatar_file_size"
     t.datetime "avatar_updated_at"
     t.string   "description"
+    t.string   "condition"
   end
 
   create_table "users", force: :cascade do |t|
